8ada92df12ea84fa03a3c2a577e539774e07891a
wdvproj <dkevpro@outlook.fr>
2022-12-04 20:16:42 +0100

    Modification des fonctions

--- front/js/script.js
+++ front/js/script.js
@@ -1,5 +1,5 @@
-/* Recupere les donnees stockees dans l'API */
-  
+/* Recuperer les donnees stockees dans l'API */
+
 async function dataCollect(url) {
   let response = await fetch(url);
   if (response.ok) {
@@ -7,73 +7,63 @@ async function dataCollect(url) {
   }
 }
 
-/* Ajoute un element enfant a un element parent */ 
+/* Creer un produit */
 
-// "childTagName" est le nom de l'element enfant, "parentTagName" est le nom de l'element parent, ce sont des chaînes de caracteres
+function createProduct(productsData) {
 
+  // Ajout du lien vers la page produit
+  let link = document.createElement("a");
+  link.setAttribute("href", "./product.html?id=" + productsData._id);
 
-function addChildIntoParent(childTagName, parentTagName) {
-  let childData;
-  let parent = document.querySelectorAll(parentTagName);
-  
-  if (parent.length === 1) {
-    childData = document.createElement(childTagName);
-    parent[0].appendChild(childData);
-  }
-  else {
-    for (let i = 0; i < parent.length; i++) {
-      let child = document.createElement(childTagName);
-      parent[i].appendChild(child);
-      if (i === 0) {
-        childData = [];
-      }
-      childData.push(child);
-    }
-  }
-  return childData;
+  // Ajout de la balise <article> 
+  let article = document.createElement("article");
+
+  // Ajout de l'image du produit
+  let picture = document.createElement("img");
+  picture.setAttribute("src", productsData.imageUrl);
+  picture.setAttribute("alt", productsData.altTxt);
+  article.appendChild(picture);
+
+  // Ajout du titre du produit
+  let title = document.createElement("h3");
+  title.setAttribute("class", "productName");
+  title.textContent = productsData.name;
+  article.appendChild(title);
+
+  // Ajout de la description du produit
+  let description = document.createElement("p");
+  description.setAttribute("class", "productDescription");
+  description.textContent = productsData.description;
+  article.appendChild(description);
+
+  link.appendChild(article);
+
+  return link;
 }
 
+/* Afficher les produits sur la page */
+
+// "productsContainer" est le selecteur du conteneur des produits, "products" est un tableau contenant l'ensemble des produits
+function displayProducts(productsContainer, products) {
+  let container = document.querySelector(productsContainer);
+  for (let product of products) {
+    container.appendChild(product);
+  } 
+}
+
+// Recupere les donnees des produits 
 dataCollect("http://localhost:3000/api/products")
-.then(function(value) {
+  .then(function(productsData) {
+    // Cree les elements DOM des produits
+    let products = [];
+    for (let productData of productsData) {
+      let product = createProduct(productData);
+      products.push(product);
+    }
 
-  // Ajout des liens vers la page produit
-
-  for (let i in value) {
-      addChildIntoParent("a", "section[id='items']").setAttribute("href", "./product.html?id=" + value[i]._id);
-  }
-
-  // Ajout des balises <article> 
-
-  addChildIntoParent("article", "a[href*='./product.html?id=']");
-
-  // Ajout des images des produits
-
-  let pictureAttribute = addChildIntoParent("img", "article");
-
-  for (let i in pictureAttribute) {
-    pictureAttribute[i].setAttribute("src", value[i].imageUrl);
-    pictureAttribute[i].setAttribute("alt", value[i].altTxt);
-  }
-
-  // Ajout des titres des produits
-
-  let titleAttributeAndContent =  addChildIntoParent("h3", "article");
-
-  for (let i in titleAttributeAndContent) {
-    titleAttributeAndContent[i].setAttribute("class", "productName");
-    titleAttributeAndContent[i].textContent = value[i].name;
-  }
-  
-  // Ajout des descriptions des produits
-
-  let descriptionAttributeAndContent =  addChildIntoParent("p", "article");
-
-  for (let i in descriptionAttributeAndContent) {
-    descriptionAttributeAndContent[i].setAttribute("class", "productDescription");
-    descriptionAttributeAndContent[i].textContent = value[i].description;
-  }
-}) 
-
-.catch(function(error) {
-  console.log("Erreur : " + error.message);
-})
+    // Affiche les produits
+    displayProducts("section[id='items']", products);
+  })
+  .catch(function(error) {
+    console.log("Erreur : " + error.message);
+  })
